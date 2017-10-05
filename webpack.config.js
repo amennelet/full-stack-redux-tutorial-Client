976ccc9d850dbf4eadcb52dc326ee4395d7ffd80
Alexandre Mennelet <amennelet@allflex-europe.com>
2017-10-05 10:22:00 +0200

    Webpack babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,22 @@ module.exports = {
     entry: [
         './src/index.js'
     ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
-/*    devServer: {
-        contentBase: '/dist'
-    }*/
+    /*    devServer: {
+            contentBase: '/dist'
+        }*/
 };
